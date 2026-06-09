f896d89f9d2dad9be50da984bef2f953518af6a6
OM0126 <omgithub01@gmail.com>
2026-06-09 11:25:04 +0530

    Add S3 bucket using Terraform

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "demo_bucket" {
+  bucket = "om0126-terraform-demo-bucket"
+
+  tags = {
+    Name = "Terraform S3 Bucket"
+  }
+}
